0b8a79e823ee67ec344d2236f6f88c1cdb270c8a
Oliveck10 <oliveck10@gmail.com>
2016-11-02 16:27:35 +0800

    first try of state

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import ReactDOM from 'react-dom';
 
 // state function (with state)
 class App extends React.Component {
+  constructor() {
+    super(); // to get this available to call
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0,
+    };
+  }
+  update(e) {
+    this.setState({ txt: e.target.value });
+  }
   render() {
    // can not return several doms => pack several nodes into single dom
     const txt = this.props.txt;
-    const cat = this.props.cat;
+    const cat = this.state.cat;
     return (
       <div>
         <h1>{txt}</h1>
         <h2>{cat}</h2>
+        <input type="text"
+          onChange={this.update.bind(this)} />
+        <h3>{`${this.props.cat} ${this.state.txt}`}</h3>
       </div>
     );
   }
@@ -26,6 +39,7 @@ App.propTypes = {
 
 App.defaultProps = {
   txt: 'this is the default txt',
+  // cat : 2,
 };
 
 ReactDOM.render(
