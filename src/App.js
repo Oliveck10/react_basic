8d49b4fdce7658938e2ea09f3182d31848e3af34
Oliveck10 <oliveck10@gmail.com>
2016-11-02 15:33:35 +0800

    first try of this.props.XXX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // stateless function
-const App = () => <h1>Hello ccc</h1>;
+// const Hello = () => <h1>Hello ccc</h1>;
 
 // state function (with state)
-// class App extends React.Component {
-//   render() {
-//     // can not return several doms => pack several nodes into single dom
-//     return <div>Hello world</div>;
-//   }
-// }
+class App extends React.Component {
+  render() {
+   // can not return several doms => pack several nodes into single dom
+    const txt = this.props.txt;
+    const cat = this.props.cat;
+    return (
+      <div>
+        <h1>{txt}</h1>
+        <h2>{cat}</h2>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired,
+};
+
+App.defaultProps = {
+  txt: 'this is the default txt',
+};
+
+ReactDOM.render(
+  <App cat={5} />, document.getElementById('app')
+);
 
 export default App;
