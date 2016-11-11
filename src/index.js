8e97b0ded8be0f89f269f823ece248af232bc141
Oliveck10 <oliveck10@gmail.com>
2016-11-11 22:00:29 +0800

    revise for demo two

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './helloWorld';
+
+// import App from './helloWorld';
+import App from './renderOneNode';
 
 
 ReactDOM.render(<App cat={5} />, document.getElementById('app'));
